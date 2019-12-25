b39e09a0980aa1b9a5b816611a015f51adf20aa0
freedzz <1425683584@qq.com>
2019-12-26 00:16:58 +0800

    first

---
+++ searchHeader/src/views/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="index">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "index",
+  data() {
+    return {
+      
+    };
+  },
+};
+</script>
+
+<style scoped>
+.index {
+  width: 100%;
+  height: calc(100% - 45px);
+}
+</style>
